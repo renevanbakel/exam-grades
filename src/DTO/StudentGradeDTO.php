179b1255eb2eb043170755e3e861dc30fa84fdd2
Rene van Bakel <renevanbakel@gmail.com>
2025-12-17 17:33:47 +0100

    Make DTO immutable

--- src/DTO/StudentGradeDTO.php
+++ src/DTO/StudentGradeDTO.php
@@ -5,10 +5,10 @@ namespace App\DTO;
 class StudentGradeDTO
 {
     public function __construct(
-        public string $studentId,
-        public float $score,
-        public float $grade,
-        public bool $passed
+        public readonly string $studentId,
+        public readonly float $score,
+        public readonly float $grade,
+        public readonly bool $passed
     ) {}
 
     public function getStudentId(): string
